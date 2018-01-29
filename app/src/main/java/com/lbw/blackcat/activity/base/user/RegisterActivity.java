5a5555ca100fe583a9836c375c741584caf93bec
lbw718733550 <718733550@qq.com>
2018-01-29 10:12:39 +0800

    注册

---
+++ app/src/main/java/com/lbw/blackcat/activity/base/user/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.lbw.blackcat.activity.base.user;
+
+import android.os.Bundle;
+
+import com.lbw.blackcat.R;
+import com.lbw.blackcat.activity.base.BaseActivity;
+
+/**
+ * @autor lbw
+ * @time 2018/1/29 10:07
+ * @desc 注册
+ */
+
+public class RegisterActivity extends BaseActivity {
+
+
+    @Override
+    public int setContentViewId() {
+        return R.layout.cat_activity_register;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
